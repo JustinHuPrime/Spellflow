cb3f65112c618c6f54721f3c6ad34f1801eb7a7d
Justin Hu <justin.hu@alumni.ubc.ca>
2024-03-07 21:03:41 -0800

    specialize template

---
+++ src/main/java/ca/zootron/spellflow/Config.java
@@ -0,0 +1,78 @@
+// Copyright 2024 Justin Hu
+//
+// This file is part of the Spellflow mod.
+//
+// The Spellflow mod is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// The Spellflow mod is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with the Spellflow mod. If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package ca.zootron.spellflow;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.config.ModConfigEvent;
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
+// Demonstrates how to use Forge's config APIs
+@Mod.EventBusSubscriber(modid = Spellflow.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Config {
+        private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+
+        private static final ModConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
+                        .comment("Whether to log the dirt block on common setup")
+                        .define("logDirtBlock", true);
+
+        private static final ModConfigSpec.IntValue MAGIC_NUMBER = BUILDER
+                        .comment("A magic number")
+                        .defineInRange("magicNumber", 42, 0, Integer.MAX_VALUE);
+
+        public static final ModConfigSpec.ConfigValue<String> MAGIC_NUMBER_INTRODUCTION = BUILDER
+                        .comment("What you want the introduction message to be for the magic number")
+                        .define("magicNumberIntroduction", "The magic number is... ");
+
+        private static final ModConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
+                        .comment("A list of items to log on common setup.")
+                        .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), Config::validateItemName);
+
+        static final ModConfigSpec SPEC = BUILDER.build();
+
+        public static boolean logDirtBlock;
+        public static int magicNumber;
+        public static String magicNumberIntroduction;
+        public static Set<Item> items;
+
+        private static boolean validateItemName(final Object obj) {
+                return obj instanceof String itemName
+                                && BuiltInRegistries.ITEM.containsKey(new ResourceLocation(itemName));
+        }
+
+        @SubscribeEvent
+        static void onLoad(final ModConfigEvent event) {
+                logDirtBlock = LOG_DIRT_BLOCK.get();
+                magicNumber = MAGIC_NUMBER.get();
+                magicNumberIntroduction = MAGIC_NUMBER_INTRODUCTION.get();
+
+                items = ITEM_STRINGS.get().stream()
+                                .map(itemName -> BuiltInRegistries.ITEM.get(new ResourceLocation(itemName)))
+                                .collect(Collectors.toSet());
+        }
+}
